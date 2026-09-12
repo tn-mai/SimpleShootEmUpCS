f6808868c21039448a9161cdb39b3e09411b8726
tanaka mai <rabitfiver@gmail.com>
2026-09-12 19:52:42 +0900

    不要なコードを除去

--- Program.cs
+++ Program.cs
@@ -9,16 +9,11 @@ namespace ShootingGameCS
     [DllImport("winmm.dll")]
     static extern uint timeBeginPeriod(uint uMilliseconds);
 
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
     [STAThread]
     static void Main()
     {
       timeBeginPeriod(1); //スリープの精度を１ミリ秒に設定
 
-      // To customize application configuration such as set high DPI settings or default font,
-      // see https://aka.ms/applicationconfiguration.
       ApplicationConfiguration.Initialize();
 
       Form1 form = new();
@@ -26,9 +21,10 @@ namespace ShootingGameCS
 
       form.Initialize();
 
+      // ゲームループ
+      double prevTotalSeconds = 0;
       Stopwatch sw = new();
       sw.Start();
-      double prevTotalSeconds = 0;
       for (; !form.IsDisposed;)
       {
         int wait = (int)((1.0 / 60.0 - (sw.Elapsed.TotalSeconds - prevTotalSeconds)) * 1000);
@@ -51,7 +47,6 @@ namespace ShootingGameCS
         form.Invalidate();
         Application.DoEvents();
       }
-      sw.Stop();
-    }
+    } // Mainメソッドブロックの終わり
   }
 }
