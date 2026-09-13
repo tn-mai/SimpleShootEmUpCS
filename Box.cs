ca233030a2734ba0290b055068f65bf1f27921ea
tanaka mai <rabitfiver@gmail.com>
2026-09-13 18:39:05 +0900

    使っていないSetメソッドを削除し、メソッド定義順を整理

--- Box.cs
+++ Box.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ShootingGameCS
 {
+  // 長方形クラス
   internal class Box
   {
     public float Left;
@@ -15,12 +16,8 @@ namespace ShootingGameCS
 
     public static readonly Box Empty = new(0, 0, 0, 0);
 
+    // コンストラクタ
     public Box(float left, float right, float top, float bottom)
-    {
-      Set(left, right, top, bottom);
-    }
-
-    public void Set(float left, float right, float top, float bottom)
     {
       Left = left;
       Right = right;
@@ -28,14 +25,16 @@ namespace ShootingGameCS
       Bottom = bottom;
     }
 
-    public static Box Add(Box box, float x, float y)
-    {
-      return new Box(box.Left + x, box.Right + x, box.Top + y, box.Bottom + y);
-    }
-
+    // 長方形同士の衝突判定
     public bool IsCollide(Box other)
     {
       return Left < other.Right && Right >= other.Left && Top < other.Bottom && Bottom >= other.Top;
+    }
+
+    // 座標(x, y)を足した新しい長方形を作る
+    public static Box Add(Box box, float x, float y)
+    {
+      return new(box.Left + x, box.Right + x, box.Top + y, box.Bottom + y);
     }
   }
 }
