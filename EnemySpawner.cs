ae3a9cd4ed1f791eb7032ec9f2e8a35b98c88bb1
tanaka mai <rabitfiver@gmail.com>
2026-09-13 18:41:48 +0900

    敵出現データをクラスにまとめる

--- EnemySpawner.cs
+++ EnemySpawner.cs
@@ -10,88 +10,101 @@ namespace ShootingGameCS
   //
   // 編隊の種類
   //   0=なし
-  //   1=直進灰色雑魚x5
-  //   2=蛇行赤雑魚x8
-  //   3=上から追尾青雑魚x4
-  //   4=Uターン緑雑魚x4
-  //   5=3Way雑魚x2
-  //   6=直進中型雑魚x1
-  //   7=蛇行中型雑魚x1
-  //   8=横から追尾青雑魚x4
-  //   9=下からUターン緑雑魚x4
-  //  10=ボス１
+  //   1=直進緑雑魚x4(出現位置ランダム、出現タイミングややランダム、上部から対角線方向(ややランダム)に直進)
+  //   2=直進緑雑魚x8(同時に8機が水平に出現(ランダムなし)、真下へ直進)
+  //   3=蛇行灰雑魚x8(0.25秒間隔で同じ位置に出現、ゆっくり目に舌へ移動しつつ左右に大きく蛇行)
+  //   4=自機狙い青雑魚x8(画面上と左右にランダムな位置にランダムなタイミングで出現)
+  //   5=Uターン赤雑魚x4(0.25秒間隔で同じ位置に出現)
+  //   6=3Way雑魚x2
+  //   7=直進中型雑魚x1
+  //   8=蛇行中型雑魚x1
+  //   9=横から追尾青雑魚x4
+  //  10=下からUターン緑雑魚x4
+  //  11=ボス１
   internal class EnemySpawner
   {
     public int Type;    // 出現させる編隊の種類
-    public int BaseX;   // 出現位置のX座標
+    public float BaseX; // 出現位置のX座標
     public int Count;   // 出現済みの敵の数
     public float Timer; // 出現間隔タイマー
 
-    private static readonly int[] enemyTypeList = { 0, 0, 6, 2, 5, 4, 7, 8, 2, 5, 9 };
-    private static readonly float[] intervalList = { 0.0f, 0.3f, 0.25f, 0.5f, 1.0f, 2.0f, 0.0f, 0.0f, 0.5f, 1.0f, 0.0f };
-    private static readonly float[][] offsetList = {
-        new float[] { },
-        new float[] { 0, -64, 64, -128, 128 },
-        new float[] { 0, 0, 0, 0, 0, 0, 0, 0 },
-        new float[] { 0, 256, -256, 128 },
-        new float[] { 0, -384, 256, -128 },
-        new float[] { 0, 384 },
-        new float[] { 0 },
-        new float[] { 0 },
-        new float[] { 320, 192, 480, 256 },
-        new float[] { 0, 192, -128, 256 },
-        new float[] { 0 },
-      };
+    private class TypeData
+    {
+      public int EnemyType;      // 敵の種類
+      public float Interval;     // 出現間隔(秒)
+      public float[] OffsetList; // 出現位置の補正量(ドット)
+
+      public TypeData(int enemyType, float interval, float[] offsetList)
+      {
+        EnemyType = enemyType;
+        Interval = interval;
+        OffsetList = offsetList;
+      }
+    }
+    private static readonly TypeData[] typeDataList = {
+      new(0, 0.0f, new float[]{ }),
+      new(0, 0.3f, new float[]{ 0, -64, 64, -128, 128 }),
+      new(6, 0.25f, new float[] { 0, 0, 0, 0, 0, 0, 0, 0 }),
+      new(2, 0.5f, new float[] { 0, 256, -256, 128 }),
+      new(5, 1.0f, new float[] { 0, -384, 256, -128 }),
+      new(4, 2.0f, new float[] { 0, 384 }),
+      new(7, 0.0f, new float[] { 0 }),
+      new(8, 0.0f, new float[] { 0 }),
+      new(2, 0.5f, new float[] { 320, 192, 480, 256 }),
+      new(5, 1.0f, new float[] { 0, 192, -128, 256 }),
+      new(9, 0.0f, new float[] { 0 }),
+    };
 
     // コンストラクタ
-    public EnemySpawner(int type, int x)
+    public EnemySpawner(int type, float x)
     {
       Type = type;
       BaseX = x;
       Count = 0;
-      Timer = intervalList[Type];
+      Timer = typeDataList[Type].Interval;
     }
 
     // 所定時間ごとに敵を発生させる
     public void Update(float deltaTime, List<Enemy> enemies, List<Enemy> bossList, Character target)
     {
       // 発生数が配列の長さを超えたらそれ以上は発生させない
-      if (Count >= offsetList[Type].Length)
+      if (Count >= typeDataList[Type].OffsetList.Length)
       {
         return;
       }
 
       // タイマーが発生間隔未満なら発生させない
       Timer += deltaTime;
-      if (Timer < intervalList[Type])
+      if (Timer < typeDataList[Type].Interval)
       {
         return;
       }
 
-      float x = BaseX + offsetList[Type][Count];
+      float offset = typeDataList[Type].OffsetList[Count];
+      float x = BaseX + offset;
       float y = -64;
       if (BaseX >= Form1.nativeWidth * 0.5f)
       {
-        x = BaseX - offsetList[Type][Count];
+        x = BaseX - offset;
       }
 
       if (Type == 8)
       {
         x = BaseX;
-        y = offsetList[Type][Count];
+        y = offset;
       }
       else if (Type == 9)
       {
         y = Form1.nativeHeight;
       }
-      Enemy e = new(x, y, enemyTypeList[Type], target);
+      Enemy e = new(x, y, typeDataList[Type].EnemyType, target);
       enemies.Add(e);
-      if (enemyTypeList[Type] == 9)
+      if (typeDataList[Type].EnemyType == 9)
       {
         bossList.Add(e);
       }
       Count++;
-      Timer -= intervalList[Type];
+      Timer -= typeDataList[Type].Interval;
     }
   } // EnemySpawnerクラスブロックの終わり
 }
